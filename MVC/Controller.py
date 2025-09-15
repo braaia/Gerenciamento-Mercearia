9a28b0c3d9d0313279e1df06927fda358771479f
Wesley Nogueira <wessleynog@gmail.com>
2025-09-15 18:15:54 -0300

    Update: Controller ADD: alterar e mostrar categoria

--- MVC/Controller.py
+++ MVC/Controller.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 class ControllerCategoria:
     def cadastroCategoria(self, novaCategoria):
-        exist = False
+        existe = False
         x = DaoCategoria.ler()
         for i in x:
             if i.categoria.lower() == novaCategoria.lower():
-                exist = True
+                existe = True
             
-        if not exist:
+        if not existe:
             DaoCategoria.salvar(novaCategoria)
             print("Categoria cadastrada com sucesso!")
         else:
@@ -33,3 +33,26 @@ class ControllerCategoria:
                 for i in x:
                     arq.writelines(i.categoria)
                     arq.writelines('\n')
+
+    def alterarCategoria(self, categoriaAlterar, novaCategoria):
+        x = DaoCategoria.ler()
+
+        cat_list = list(filter(lambda x: x.categoria.lower() == categoriaAlterar.lower(), x))
+
+        if len(cat_list) > 0:
+            cat1_list = list(filter(lambda x: x.categoria.lower() == novaCategoria.lower(), x))
+            if len(cat1_list) == 0:
+                x = list(map(lambda x: Categoria(novaCategoria) if(x.categoria.lower() == categoriaAlterar.lower()) else(x), x))
+            else:
+                print("A categoria que você deseja alterar já existe!")
+        else:
+            print("A Categoria que você deseja alterar não existe!")
+
+    def mostrarCategoria(self):
+        categorias = DaoCategoria.ler()
+
+        if len(categorias) <= 0:
+            print("Categoria vazia!")
+        else:
+            for i in categorias:
+                print(f"Categoria: {i.categoria}")
